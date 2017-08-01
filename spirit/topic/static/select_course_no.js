978432f96719a248cc65e75ea00618d1658c76d0
Marika Swanberg <marlswanb@reed.edu>
2017-08-01 10:48:19 -0700

    added background photo to forum

--- spirit/topic/static/select_course_no.js
+++ spirit/topic/static/select_course_no.js
@@ -1,8 +1,8 @@
 // This is a function that will be used by spirit/topic/_top_bar.html
-// When someone clicks 
+// When someone clicks
 
 
 function go_to_course_number(url) {
   var no = document.getElementById("course_no").value
-  window.open(url + no, '_self', false) 
+  window.open(url + no, '_blank', false) 
 }
